6ea36c4dbc54e174eacf8e1801212eba69bc69ff
Mahardika Bayu Rahmadi <Mahardika.Bayu.Rahmadi@students.untidar.ac.id>
2025-07-14 23:23:15 +0700

    sdk corrction ii

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.himatif_list_01"
-    compileSdk = 21
+    compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
     compileOptions {
